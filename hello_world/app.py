23d60c36631ea546ba77841d1ea74d053570d057
Akira <freelance0918@gmail.com>
2022-10-16 09:51:27 +0900

    add access coincheck

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,38 +1,23 @@
 import json
-
+import boto3
+import ccxt
 # import requests
+REGION = 'ap-northeast-1'
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
+    ssm = boto3.client('ssm', region_name=REGION)
+    response = ssm.get_parameters(
+        Names=[
+            'coincheck-api-key',
+            'coincheck-secret',
+        ],
+        WithDecryption=True
+    )
+    apikey = response['Parameters'][0]['Value']
+    apisecret = response['Parameters'][1]['Value']
+    coincheck = ccxt.coincheck({'apiKey': apikey, 'secret': apisecret})
+    print(coincheck.fetch_ticker(symbol='BTC/JPY'))
     return {
         "statusCode": 200,
         "body": json.dumps({
