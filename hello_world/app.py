4d7e7d7d4598dc5ca02904f587509b8864ccbda1
Akira <freelance0918@gmail.com>
2022-10-16 13:45:58 +0900

    add ifdone

--- hello_world/app.py
+++ hello_world/app.py
@@ -23,25 +23,29 @@ def lambda_handler(event, context):
     sell = int(last*1.03)
     print(ticker['info']['last'])
     balance = coincheck.fetchBalance()
-    print(balance['info']['jpy'])
+    now_btc = float(balance['info']['btc'])
     order = coincheck.create_order(
         symbol='BTC/JPY',
         type='limit',
         side='buy',
-        amount=0.00004,
+        amount=0.005,
         price=buy,
     )
-    order2 = coincheck.create_order(
-        symbol='BTC/JPY',
-        type='limit',
-        side='sell',
-        amount=0.0039,
-        price=sell,
-    )
+    if now_btc >= 0.005:
+        order2 = coincheck.create_order(
+            symbol='BTC/JPY',
+            type='limit',
+            side='sell',
+            amount=0.005,
+            price=sell,
+        )
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello world",
+            'order_buy': order,
+            'order_sell': order2
+
             # "location": ip.text.replace("\n", "")
         }),
     }
